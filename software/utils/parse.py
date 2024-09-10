90964739e71c96e9f5b6efe586c601a3221b69ed
connorl309 <cleu309fw@gmail.com>
2024-09-10 13:06:22 -0500

    quick python parse of z80inst.txt

---
+++ software/utils/parse.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+from typing import List
+import re
+
+f = open("./instructions.txt", "r")
+
+alu_ops = ["ADD", "ADC", "SUB", "SBC", "AND", "OR", "XOR", "CP"]
+
+def translate(input_line):
+    output_list = []
+    for item in input_line:
+        # Split the input by '/'
+        #parts = item.split('/')
+        # Use regex to capture the text before parentheses and the number inside parentheses
+        match = re.match(r'([A-Z]+)\((\d+)\)', item)
+        if match:
+            prefix, number = match.groups()
+            output_list.append(f"STATE_{prefix}_{number}")
+    
+    return output_list
+
+lines = [l.lstrip() for l in f]
+lines = list(filter(None, lines))
+for a in lines:
+    split = a.split("\t")
+    split = list(filter(None, split))
+    if len(split) < 2:
+        print("Manual microcode settings required: " + str(split))
+        None
+    else:
+        newlist = split[2:]
+        if ("ALU" in split[0]):
+            for op in alu_ops:
+                res = translate(newlist)
+                print("{0:15}".format(split[0]), end="\t\t")
+                for i in res:
+                    print(i, end='\t')
+                print()
+        else:
+            res = translate(newlist)
+            print("{0:15}".format(split[0]), end="\t\t")
+            for i in res:
+                print(i, end='\t')
+            print()
