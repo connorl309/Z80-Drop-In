28b5b8219eca1c9bf32beed7a41139dd1a051b16
April <maprilbear@gmail.com>
2025-01-23 21:34:48 -0600

    Logging example that works on Narvi board

---
+++ z80_rtl/z80_rtl.srcs/sources_1/new/top.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/23/2025 06:31:46 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    input CLK1,
+    input RESET
+    );
+    
+    design_1 temp(CLK1, RESET);
+endmodule
