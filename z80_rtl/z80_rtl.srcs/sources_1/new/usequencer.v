bed032ea1957080415b46a0431ae6bfb8bd4bf0b
Nadia <nadiahouston@utexas.edu>
2025-02-26 19:41:41 +0000

    Fix bug with external control signals set on rising edge

--- z80_rtl/z80_rtl.srcs/sources_1/new/usequencer.v
+++ z80_rtl/z80_rtl.srcs/sources_1/new/usequencer.v
@@ -46,7 +46,7 @@ module usequencer(
     output ext_wr,
     output reg ext_halt,
 
-    output [48:0] m_signals, // Not certain about the number
+    output [49:0] m_signals,
     output [`TSIGNALS:0] t_signals
     );
 
@@ -78,7 +78,7 @@ module usequencer(
 
     // Decode logic
 
-    wire [244:0] exec_sigs;
+    wire [249:0] exec_sigs;
     wire [1:0] PLA_idx_w;
     wire [24:0] m_states;
     wire [2:0] max_m_cycles_w;
@@ -94,7 +94,7 @@ module usequencer(
     reg [1:0] IX_pref = 2'b0;
     reg [1:0] IY_pref = 2'b0;
 
-    decode dec(IR, PLA_idx, IX_pref[1], IY_pref[1], exec_sigs, PLA_idx_w, m_states,
+    decode dec(IR, PLA_idx, IX_pref[1], IY_pref[1], exec_sigs, nmi, PLA_idx_w, m_states,
                      max_m_cycles_w, f_stall, next_IX_pref, next_IY_pref, no_int);
 
     upcoming_m_cycles umc(t_signals[`CS_SET], m_states, exec_sigs, m_cycle_ctr,
@@ -141,7 +141,7 @@ module usequencer(
         t_state <= next_t_state;
 
         // Update output latches only if not waiting
-        if(!(t_signals[`COND0] && wait_latch)) begin
+        if(!(t_signals[`COND0] && !wait_latch)) begin
             if(t_signals[`MREQ_S_RE])
                 mreq <= 1;
 
@@ -253,16 +253,19 @@ endmodule
 module upcoming_m_cycles(
     input cs_set,
     input [24:0] m_states,
-    input [244:0] exec_signals,
+    input [249:0] exec_signals,
     input [2:0] index,
-    output reg [4:0] next_m_state,
-    output reg [48:0] curr_exec_signals
+    output [4:0] next_m_state,
+    output [49:0] curr_exec_signals
     );
 
     reg [4:0] m_state_array [4:0];
     reg [49:0] exec_array [4:0];
 
-    always @(cs_set or index) begin
+    assign next_m_state = m_state_array[index+1];
+    assign curr_exec_signals = exec_array[index];
+
+    always @(cs_set) begin
         // There is probably a better way to do this but I am a bit simple
         m_state_array[0] <= m_states[4:0];
         m_state_array[1] <= m_states[9:5];
@@ -270,17 +273,12 @@ module upcoming_m_cycles(
         m_state_array[3] <= m_states[19:15];
         m_state_array[4] <= m_states[24:20];
 
-        exec_array[0] <= exec_signals[48:0];
-        exec_array[1] <= exec_signals[97:49];
-        exec_array[2] <= exec_signals[146:98];
-        exec_array[3] <= exec_signals[196:147];
-        exec_array[4] <= exec_signals[244:197];
-
-        next_m_state <= m_state_array[index+1];
-        curr_exec_signals <= exec_array[index];
+        exec_array[0] <= exec_signals[49:0];
+        exec_array[1] <= exec_signals[99:50];
+        exec_array[2] <= exec_signals[149:100];
+        exec_array[3] <= exec_signals[199:150];
+        exec_array[4] <= exec_signals[249:200];
     end
-
-
 endmodule
 
 module flag_logic(
@@ -303,78 +301,3 @@ module flag_logic(
     end
 endmodule
 
-/* Used for testing, no longer needed
-
-module dummy_decode(
-    input [7:0] IR,
-    input [1:0] PLA_idx,
-    input IX_pref, IY_pref,
-    output reg [279:0] exec_signals,
-    output reg [1:0] next_PLA,
-    output reg [34:0] m_states,
-    output reg [2:0] max_m_cycles,
-    output reg [1:0] f_stall,
-    output reg next_IX_pref, next_IY_pref,
-    output reg no_int
-    );
-
-    always @(*) begin
-        no_int = IX_pref; // only here so iverilog doesn't get mad at me
-        exec_signals = 280'b0;
-        next_PLA = 2'b0;
-        m_states = 35'b0;
-        max_m_cycles = 3'b0;
-        f_stall = 2'b0;
-        next_IX_pref = 1'b0;
-        next_IY_pref = 1'b0;
-        no_int = 1'b0;
-    end
-
-endmodule
-
-
-module dummy_t_cs(
-    input [6:0] state_num,
-    output reg [`TSIGNALS:0] t_signals
-    );
-
-    always @* begin
-        t_signals = `TSIGNALS'b0;
-
-        // OCF
-        if(state_num == 0) begin
-            t_signals[`JBITS] = 7'd30;
-            t_signals[`MREQ_R] = 1;
-            t_signals[`RD_R] = 1;
-            t_signals[`M1] = 1;
-        end else if(state_num == 30) begin
-            t_signals[`JBITS] = 7'd30;
-            t_signals[`COND0] = 1;
-            t_signals[`M1] = 1;
-            t_signals[`MREQ_S_RE] = 1;
-            t_signals[`RD_S_RE] = 1;
-        end else if(state_num == 31) begin
-            t_signals[`JBITS] = 7'd32;
-            t_signals[`COND1] = 1;
-            t_signals[`CS_SET] = 1;
-            t_signals[`MREQ_R] = 1;
-            t_signals[`RFSH] = 1;
-        end else if(state_num == 36) begin
-            t_signals[`JBITS] = 7'd34;
-        end else if(state_num == 34) begin
-            t_signals[`JBITS] = 7'd32;
-        end else if(state_num == 32) begin
-            t_signals[`RFSH] = 1;
-            t_signals[`MREQ_S_FE] = 1;
-            t_signals[`EXEC] = 1;
-            t_signals[`LAST_T] = 1;
-        end else if(state_num == 70) begin
-            t_signals[`BUSACK] = 1;
-            t_signals[`LAST_T] = 1;
-        end
-
-    end
-
-endmodule
-
-*/
